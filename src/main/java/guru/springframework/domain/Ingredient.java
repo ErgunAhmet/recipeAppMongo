efd2bc0a76ace00b54d5e60618fc6b9446974b03
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-05-23 01:16:24 +0200

    setup

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -2,20 +2,25 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * Created by jt on 6/13/17.
  */
 @Getter
 @Setter
+@Document
 public class Ingredient {
 
-    private String id;
+    private String id = UUID.randomUUID().toString();
     private String description;
     private BigDecimal amount;
-
+    @DBRef
     private UnitOfMeasure uom;
     private Recipe recipe;
 
